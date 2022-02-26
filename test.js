ff15271dc9186850e70ecf3cfb16d2a28fca4c8e
Mikhail Shornikov <93269956+mshornikov@users.noreply.github.com>
2022-02-26 21:17:34 +0300

    Now HTNL markup is writing in JS. And now HTML markup depends from lesson classes in JS

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+// Classes
+
 class Time {
     constructor(hours, minutes, seconds) {
         this.hours = hours;
@@ -64,6 +66,8 @@ class Lesson {
     }
 }
 
+// Lessons declaration
+
 var mondayLessons = [new Lesson('Математика', new Time(8, 15, 0), new Time(8, 55, 0)), new Lesson('Химия', new Time(9, 5, 0), new Time(9, 45, 0)), new Lesson('Математика', new Time(10, 5, 0), new Time(10, 45, 0)), 
 new Lesson('Обществознание', new Time(11, 0, 0), new Time(11, 40, 0)), new Lesson('Литература', new Time(11, 50, 0), new Time(12, 30, 0)), new Lesson('Английский язык', new Time(12, 40, 0), new Time(13, 20, 0)),
 new Lesson('ОБЖ', new Time(13, 30, 0), new Time(14, 10, 0))];
@@ -83,6 +87,8 @@ var fridayLessons = [new Lesson('Физкультура', new Time(8, 15, 0), ne
 new Lesson('Родной язык', new Time(11, 0, 0), new Time(11, 40, 0)), new Lesson('Экономика', new Time(11, 50, 0), new Time(12, 30, 0)), new Lesson('Биология', new Time(12, 40, 0), new Time(13, 20, 0)),
 new Lesson('История', new Time(13, 30, 0), new Time(14, 10, 0))];
 
+// Days declaration
+
 var sunday = new Day('Воскресенье', 0, [])
 var monday = new Day('Понедельник', 7, mondayLessons);
 var tuesday = new Day('Вторник', 7, tuesdayLessons);
@@ -91,30 +97,47 @@ var thursday = new Day('Четверг', 6, thursdayLessons);
 var friday = new Day('Пятница', 7, fridayLessons);
 var saturday = new Day('Суббота', 0, [])
 
-var days = [sunday, monday, tuesday, wednesday, thursday, friday, saturday];
+// Week declaration
 
-function getWeekDay(date, days) {    
-    return days[date.getDay()];
+var week = [sunday, monday, tuesday, wednesday, thursday, friday, saturday];
+
+// Function which changing '1' -> '01'
+
+function zero_first_format(value)
+{
+    if (value < 10)
+    {
+        value='0'+value;
+    }
+    return value;
+}
+
+// Function which replacing day number to day object ('0' -> sunday)
+
+function getWeekDay(date, week) {    
+    return week[date.getDay()];
     } 
 
+// Function whic by current time understans what lesson now
+
 function whatLessonNow() {
+
+    // Current time declaration
     var current_datetime = new Date();
     var hours = current_datetime.getHours();
     var minutes = current_datetime.getMinutes();
     var seconds = current_datetime.getSeconds();
-    var day = getWeekDay(current_datetime, days);
+    var day = getWeekDay(current_datetime, week);
 
-    // var hours = 8;
-    // var minutes = second();
-    // var seconds = 0;
-    // var day = monday;
-
+    // Converting time from 8:01:01 to 28861 for comparing
     let time = hours * 3600 + minutes * 60 + seconds;
 
+    // Weekend
     if (day == saturday | day == sunday) {
         return 'Сегодня выходной';
     }
 
+    // Loop which
     for (i in day.get_lessons()){
         if (time >= day.get_lessons()[i].get_startTime().get_time() & time <= day.get_lessons()[i].get_endTime().get_time()) {
             return 'Сейчас: ' + day.get_lessons()[i].get_name();
@@ -125,49 +148,47 @@ function whatLessonNow() {
         
     }
 
+    // Time after lessons ('15:00')
     if (time >= day.get_lessons()[day.get_lessons().length - 1].get_endTime().get_time() ) {
         return 'Уроки уже закончились';
     }
 
+    // Time before lessons ('8:14')
     if (time <= day.get_lessons()[0].get_endTime().get_time()) {
         return 'Уроки ещё не начались. <br> Первый урок: ' + day.get_lessons()[0].get_name();
     }
 
+    // Break
     return 'Перемена. Cледующий урок: ' + nextLesson;
 
 }
 
-
-// function whatLesson(hours, minutes, seconds, day) {
-
-//     let time = hours * 3600 + minutes * 60 + seconds;
-
-//     if (day == saturday | day == sunday) {
-//         return 'Сегодня выходной';
-//     }
-
-//     for (i in day.get_lessons()){
-//         if (time >= day.get_lessons()[i].get_startTime().get_time() & time <= day.get_lessons()[i].get_endTime().get_time()) {
-//             return 'Сейчас: ' + day.get_lessons()[i].get_name();
-//         }
-//         if (time + 1200 >= day.get_lessons()[i].get_startTime().get_time()) {
-//             var nextLesson = day.get_lessons()[i].get_name();
-//         }
-        
-//     }
-
-//     if (time >= day.get_lessons()[day.get_lessons().length - 1].get_endTime().get_time() ) {
-//         return 'Уроки уже закончились';
-//     }
-
-//     if (time <= day.get_lessons()[0].get_endTime().get_time()) {
-//         return 'Уроки ещё не начались. <br> Первый урок: ' + day.get_lessons()[0].get_name();
-//     }
-
-//     return 'Перемена. Cледующий урок: ' + nextLesson;
-
-// }
+// Updating status in header
 
 setInterval(function () {
     document.getElementById('current_status').innerHTML = whatLessonNow();
 }, 100);
+
+// Document writing
+document.write('<div class="container">');
+document.write('<div class="week">');
+for (i in week) {
+    if (i != 0 & i != 6) {
+        document.write('<article class="day">');
+        document.write('<section class="day-header">', week[i].get_dayName(), '</section>');
+        
+        for (x in week[i].get_lessons()) {
+            document.write('<section class="lesson-line">');
+            document.write('<section class="lesson-start-time">', week[i].get_lessons()[x].get_startTime().get_hours(), ':', zero_first_format(week[i].get_lessons()[x].get_startTime().get_minutes()), '</section>');
+            document.write('<section class="lesson-column">');
+            document.write('<section class="lesson-name">', week[i].get_lessons()[x].get_name(), '</section>');
+            document.write('<section class="lesson-end-time">', week[i].get_lessons()[x].get_endTime().get_hours(), ':', zero_first_format(week[i].get_lessons()[x].get_endTime().get_minutes()), '</section>');
+            document.write('</section>');
+            document.write('</section>');
+        }
+
+        document.write('</article>');
+    }
+} 
+document.write('</div>');
+document.write('</div>');
